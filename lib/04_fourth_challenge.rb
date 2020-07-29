3cc223a543c0240d6d9f14285338f596ffee0989
Cannigia Laluw <cannigialaluw@gmail.com>
2020-07-29 21:40:34 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -8,7 +8,7 @@ def fourth_challenge
         {
         :name => "Benvolio",
         :age => "17",
-        :attitude => "worried"}
+        :attitude => "worried"},
         {
         :name => "Mercutio",
         :age => "18",
@@ -23,7 +23,7 @@ def fourth_challenge
         {
         :name => "Steven",
         :age => "30",
-        :attitude => "confused"}
+        :attitude => "confused"},
         {
         :name => "Nurse",
         :age => "44",
@@ -31,7 +31,5 @@ def fourth_challenge
       ]
    }
 }
-
-  
 
 end
